fb21d29e51ff21d14033a60a7d1e1983263286c6
b277zhan <b277zhan@student.cs.uwaterloo.ca>
2018-05-10 16:55:33 -0400

    A0: Added a Kernel Menu Command - dth

--- kern/startup/menu.c
+++ kern/startup/menu.c
@@ -396,6 +396,20 @@ cmd_kheapstats(int nargs, char **args)
 	return 0;
 }
 
+/*
+ * Command for running dth.
+ */
+static
+int
+cmd_dth(int nargs, char **args)
+{
+	(void)nargs;
+	(void)args;
+
+	dbflags = DB_THREADS;
+
+	return 0;
+}
 ////////////////////////////////////////
 //
 // Menus.
@@ -437,6 +451,7 @@ static const char *opsmenu[] = {
 	"[sync]    Sync filesystems          ",
 	"[panic]   Intentional panic         ",
 	"[q]       Quit and shut down        ",
+  "[dth]     Enable DB_THREADS debugging msgs",
 	NULL
 };
 
@@ -549,6 +564,7 @@ static struct {
 	{ "q",		cmd_quit },
 	{ "exit",	cmd_quit },
 	{ "halt",	cmd_quit },
+  { "dth", cmd_dth },
 
 #if OPT_SYNCHPROBS
 	/* in-kernel synchronization problem(s) */
